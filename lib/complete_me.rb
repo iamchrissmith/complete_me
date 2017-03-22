23224a9e670162b9482cc7e779890e61776f37a9
Chris Smith <42@chrisryansmith.com>
2017-03-22 08:10:08 -0600

    suggestions working, tests cleared

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -9,11 +9,9 @@ class CompleteMe
   end
 
   def count
-    # binding.pry
     count_of_each = []
     current = @head
     stack = current.children.map do |key, value|
-      # stack.push(current.children[key])
       value
     end
     until stack.empty?
@@ -43,66 +41,29 @@ class CompleteMe
 
   def suggest(partial)
     letters = partial.split('')
-    find_suggestion_start(letters, node = @head)
+    start_node = find_suggestion_start(letters)
+    return "No Suggestions" unless start_node
+    final = traverse_suggestions(partial, start_node)
+    final.flatten
+  end
+
+  def traverse_suggestions (prefix, node = @head, suggestions = [])
+    return prefix if node.word_end != 0
+
+    node.children.each do |key, child|
+      suggestions << traverse_suggestions(prefix + child.letter, child)
+    end
+    suggestions
   end
 
   def find_suggestion_start(letters, node = @head, suggestion = '')
     letter = letters[0]
-    return "No Suggestions" if !node.find_node(letter)
+    return false if !node.find_node(letter)
     suggestion += letters.shift
-    # binding.pry
     if letters.length > 0
       find_suggestion_start(letters, node.children[letter.to_sym],suggestion)
     else
-      # binding.pry
-      # collected_suggestion = []
-      collected_suggestion = collect_suggestions(node.children[letter.to_sym])
-      # binding.pry
-      final = []
-      final << parse_collected( suggestion, collected_suggestion )
-      # [suggestion.join]
+      node.children[letter.to_sym]
     end
-
   end
-
-  def parse_collected(root, collected)
-    binding.pry
-    collected.each do |key,value|
-      root = root + key.to_s
-      root = parse_collected(root, value)
-    end
-    root
-    # until collected.empty?
-    #   root = root + collected.shift.to_s
-    #   if collected[0].length > 1
-    #     root = parse_collected(root,collected[1])
-    #   else
-    #     root = root + collected[1].to_s
-    #   end
-    # end
-    # root = collected.reduce(0) { |memo, (key, value)| memo += key.to_s }
-  end
-
-  def collect_suggestions(node, suggestion={})
-    # binding.pry
-    unless node.children.empty?
-      node.children.each do |key, child|
-        suggestion[key] = collect_suggestions(child, suggestion[key] = {})
-      end
-    end
-    suggestion
-  end
-
-  # def find_letters(partial, suggestion = [''])
-  #   letter = partial[0]
-  #   # binding.pry
-  #   return "No Suggestions" if !find_node(letter)
-  #   suggestion << partial.shift
-  #   children[letter.to_sym].find_letters(partial, suggestion)
-  #   # suggestion = [suggestion[0] + letter]
-  #   # binding.pry
-  #   # look_down(partial, suggestion)
-  # end
-
-
 end
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -42,7 +42,6 @@ class CompleteMeTest < MiniTest::Test
   end
 
   def test_find_letters_returns_no_suggestions_if_not_found
-    skip
     completion = CompleteMe.new
 
     result = completion.suggest("a")
@@ -56,25 +55,7 @@ class CompleteMeTest < MiniTest::Test
     assert_equal expected, result_2
   end
 
-  def test_find_letters_returns_array_of_found_letters
-    skip
-    completion = CompleteMe.new
-    completion.insert('a')
-    assert_equal "a", completion.head.children[:a].letter
-
-    completion.head.children[:a].add_node('a')
-    assert_equal "a", completion.head.children[:a].children[:a].letter
-
-    completion.head.children[:a].children[:a].add_node('a')
-    assert_equal "a", completion.head.children[:a].children[:a].children[:a].letter
-
-    result = completion.suggest("aaa")
-    expected = ["aaaa"]
-    assert_equal expected, result
-  end
-
   def test_suggest_returns_array_of_options_one_word
-    skip
     completion = CompleteMe.new
     completion.insert("pizza")
     result = completion.suggest("piz")
@@ -82,7 +63,6 @@ class CompleteMeTest < MiniTest::Test
   end
 
   def test_suggest_returns_array_of_options_more_than_one_word
-    skip
     completion = CompleteMe.new
     completion.insert("pizza")
     completion.insert("pizzeria")
@@ -90,36 +70,12 @@ class CompleteMeTest < MiniTest::Test
     assert_equal ["pizza", "pizzeria"], result
   end
 
-  def test_add_suggestion_letter_returns_with_letter_appended
-    skip
-    node = Node.new('')
-    result = node.add_suggestion_letter("a", ["aaa", "bbb", "ccc"])
-    expected = ["aaaa", "bbba", "ccca"]
-    assert_equal expected, result
-  end
-
-  def test_branch_suggestions_creates_array_for_children
-    skip
-    node = Node.new('')
-    result = node.add_suggestion_letter("aaa", ["", "", ""])
-    expected = ["aaa", "aaa", "aaa"]
-    assert_equal expected, result
-  end
-
-  def test_populate_suggestions_returns_suggestion_if_no_children
-    skip
-    node = Node.new('')
-    result
-  end
-
   def test_populate_adds_whole_dictionary
-    # skip
+    skip
     completion = CompleteMe.new
     dictionary = File.read("/usr/share/dict/words")
-    # dictionary = file.read
     completion.populate(dictionary)
     assert_equal 235886, completion.count
-    # puts completion.count
   end
 
 end
